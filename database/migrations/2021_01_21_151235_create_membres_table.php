666648d85a40815f1ad96196012268f9d1900ce0
HaLan0101 <dnhalan0101@gmail.com>
2021-01-21 16:48:10 +0100

    BDD version 1

---
+++ database/migrations/2021_01_21_151235_create_membres_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membres', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('mdp');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('date_de_naissance');
+            $table->integer('solde');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membres');
+    }
+}
