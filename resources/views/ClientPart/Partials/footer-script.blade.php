40fb553831be87203728765b165874e1c733d0fa
TaykoWS <gto971972@outlook.com>
2021-01-22 19:39:58 +0100

    Version 2 - IntegrationClient
    Layout du FrontEnd Client prêt. Liens à faire entre les différentes pages.

--- resources/views/ClientPart/Partials/footer-script.blade.php
+++
@@ -1,2 +0,0 @@
-<script src="{{ asset('startbootstrap-heroic-features-gh-pages\vendor\jquery\jquery.min.js') }}"></script>
-<script src="{{ asset('startbootstrap-heroic-features-gh-pages\vendor\bootstrap\js\bootstrap.bundle.min.js') }}"></script>
